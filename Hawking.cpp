8a885871d8f9ccdf9bc2268d0bcfd49cfeda9d34
Kacper Bojarczuk <kacper.bojarczuk1@gmail.com>
2023-04-24 22:22:04 +0200

    exercises

---
+++ Hawking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string unformatted_full_name{"StephenHawking"};
+
+    string first_name = unformatted_full_name.substr(0, 7);
+    string last_name = unformatted_full_name.substr(7, 7);
+    string formatted_full_name = first_name + last_name;
+    formatted_full_name.insert(7, " ");
+
+    cout << formatted_full_name;
+
+    return 0;
+}
---
+++ Newton.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string journal_entry_1{"Isaac Newton"};
+    string journal_entry_2{"Leibniz"};
+
+    journal_entry_1.erase(0, 6);
+    if (journal_entry_2 < journal_entry_1)
+        journal_entry_2.swap(journal_entry_1);
+
+    cout << journal_entry_1 << "\n"
+         << journal_entry_2;
+
+    return 0;
+}
