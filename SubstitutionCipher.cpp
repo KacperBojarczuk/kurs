136516dd7dc7b14f022ed2cc265532183249e065
Kacper Bojarczuk <kacper.bojarczuk1@gmail.com>
2023-04-25 15:56:53 +0200

    finished

--- SubstitutionCipher.cpp
+++ SubstitutionCipher.cpp
@@ -3,33 +3,41 @@
 #include <string>
 using namespace std;
 
-/*
-abc zamienia sie na ZEB
-
-
-
-
-*/
-
 int main()
 {
     string alphabet{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
     string key={"ZEBRASCDFGHIJKLMNOPQTUVWXYzebrascdfghijklmnopqtuvwxy"};
     string code;
     
-    cout << "Enter secret code: ";
+    cout << "Enter secret message: ";
     getline(cin, code);
+
+    cout<<"Encrypting message..."<<endl;
 
     for (int i{0}; i < code.size(); i++)
     {
-        int position = alphabet.find(code.at(i));
+        int position = alphabet.find(code.at(i)); //znajdz pozycje znaku w 'alphabet' odpowiadajacej pozycji znaku w 'code'
+        
         if (position != string::npos)
         {
             code.at(i) = key.at(position);
         }
     }
+    
+    cout <<"Encrypted message: "<<code<<endl;
 
-    cout << code;
+    cout<<"Decrypting message..."<<endl;
+
+    for (int i{0}; i < code.size(); i++)
+    {
+        int position = key.find(code.at(i));
+        if (position != string::npos)
+        {
+            code.at(i) = alphabet.at(position);
+        }
+    }
+
+    cout <<"Decrypted message: "<<code<<endl;
 
     return 0;
 }
